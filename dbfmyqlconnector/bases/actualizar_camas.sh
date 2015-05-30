d8c57fa38a9232ad8722437b7aeeeb61dd85f3b3
rodrigo <rodrigo@asteriskubuntu.(none)>
2015-05-30 16:50:28 -0400

    DBF a MYSQL TABLA CAMAS

---
+++ dbfmyqlconnector/bases/actualizar_camas.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USER="root"
+PASSWORD="rod161082"
+DATABASE="sanlucastest1"
+TABLEFILE="camas.sql"
+HOSTNAME="localhost"
+TABLENAME="CAMAS"
+DBFFILE="CAMAS.DBF"
+
+
+#########################
+echo "Eliminando Tabla anterior"
+mysql -u$USER -p$PASSWORD $DATABASE -e "drop table $TABLENAME"
+
+echo "Creando Tabla $TABLEFILE"
+mysql -u$USER -p$PASSWORD $DATABASE < $TABLEFILE
+
+echo "IMPORTANDO DATOS DESDE $DBFFILE A $TABLENAME"
+dbf2mysql -h $HOSTNAME -d $DATABASE -U $USER -P $PASSWORD -t $TABLENAME $DBFFILE  -vvvvv
